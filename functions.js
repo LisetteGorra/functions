d9d76f4fd35479d3f7b27b895bbcf9b9c56da47a
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-05-25 18:42:10 -0400

    Created function for total bill, passing 2 parameters for daily rate and total days

--- functions.js
+++ functions.js
@@ -1,10 +1,17 @@
 function hiName(name) {
     console.log("Hi", name +".");
+    var welcome = "Welcome to the Intercontinental Hotel.";
 }
 
 hiName("Lisette");
 
 function goodBye(name) {
     console.log("Bye", name+ "!");
+        var farewell = "Thank you for visiting the Intercontinental Hotel.";
+
 }
 goodBye("Ms. Gorra");
+
+function hotelBill(dailyRate, totalDays){
+    
+}
