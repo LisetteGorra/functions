d713cce41907f4720f2cd4c00e69be27f20b6da5
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-05-25 18:47:14 -0400

    Created var sum with equation that will calculate total bill when arguments are passed in called function

--- functions.js
+++ functions.js
@@ -7,11 +7,14 @@ hiName("Lisette");
 
 function goodBye(name) {
     console.log("Bye", name+ "!");
-        var farewell = "Thank you for visiting the Intercontinental Hotel.";
+        var farewell = "Thank you for visiting the Intercontinental/Standard Hotel.";
 
 }
 goodBye("Ms. Gorra");
 
 function hotelBill(dailyRate, totalDays){
-    
+    var sum = dailyRate * totalDays;
+    console.log("Your total bill is:", sum);
 }
+
+hotelBill(450, 3);
