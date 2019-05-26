6884caa7f04be3b9f3e562185071e5ae9395a815
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-05-26 18:51:50 -0400

    Created program that allows functiont to calcualte a bill, by daily rate, and total days sum

--- functions.js
+++ functions.js
@@ -7,11 +7,23 @@ hiName("Lisette");
 
 function goodBye(name) {
     console.log("Bye", name+ "!");
-        var farewell = "Thank you for visiting the Intercontinental Hotel.";
+        var farewell = "Thank you for visiting the Intercontinental/Standard Hotel.";
 
 }
 goodBye("Ms. Gorra");
 
 function hotelBill(dailyRate, totalDays){
-    
+    var sum = dailyRate * totalDays;
+    console.log("Your total bill is: $"+ sum);
 }
+
+hotelBill(450, 3);
+
+// Your total bill for 3 days in the Camaro 2ss is $????
+
+function camaroBill (dailyRate, totalDays) {
+    var sum = dailyRate * totalDays;
+    console.log("Your total bill for 3 days in the Camaro 2ss is $" + sum);
+}
+
+camaroBill(235, 3);
